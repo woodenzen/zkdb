2301bda899bc7fe973443af884e2888e8d6a5d27
Will Simpson <will@kestrelcreek.com>
2021-11-15 20:59:41 -0800

    Simplified to stats and review

--- zkdashboard.py
+++ zkdashboard.py
@@ -11,23 +11,25 @@ import random
 from dateutil.relativedelta import relativedelta
 import pyperclip
 import argparse
+# from archive_path import TheArchive
 
 
 # With argparse running this from command line
 # python zkdashboard.py -z /Users/will/Dropbox/zettelkasten/ -c 2 -s "/Users/will/Dropbox/zettelkasten/Super Slogans 202012281549.md"
 
 parser = argparse.ArgumentParser(description='Zettelkasten Dashboard')
-parser.add_argument('-z',
-                    metavar='zettelkasten directory', type=pathlib.Path)
-parser.add_argument('-s', metavar='slogans',
-                    help='File location', type=pathlib.Path)
-parser.add_argument('-c', metavar='number',
-                    help='Number of slogans to print', type=int)
+parser.add_argument('-s', metavar='Stats',
+                    help='Just Basic Stats')
+parser.add_argument('-ra',
+                    metavar='Review', help='Archive style links')
+parser.add_argument('-rmd', metavar='Review',
+                    help='Markdown style links.')
 args = parser.parse_args()
+
 
 # path to zettelkasten
 
-target_dir = args.z  # /Users/will/Dropbox/zettelkasten/
+target_dir = pathlib.Path("/Users/will/Dropbox/zettelkasten/")
 
 # Regex
 
@@ -36,12 +38,6 @@ link_pattern = re.compile(r"(?<!{UUID_sign})\[\[.*?\d{8}]]")
 
 # Initialize a lot of counters
 
-tbooks = 0
-tblogs = 0
-tarticles = 0
-tproof = 0
-ttwodo = 0
-tvideo = 0
 twords = 0
 tlinks = 0
 tzettel = 0
@@ -54,26 +50,7 @@ two_yrs_ago_count = 0
 tengap = 10
 hundredgap = 100
 day0 = date(2018, 11, 14)
-
-
-# Death Watch
-
-birth = date(1956, 9, 26)
 today = date.today()
-eighty = date(2036, 9, 26)
-covid = date(2020, 3, 14)
-since = today - birth
-until = eighty - today
-quarintine = today - covid
-
-# Random Super Slogan
-
-with open(args.s) as file_in:  # /Users/will/Dropbox/zettelkasten/Super Slogans 202012281549.md
-    lines = []
-    for line in file_in:
-        lines.append(line[:-1])
-    sample = random.sample(lines, args.c)
-ss = ('\n'.join(sample))
 
 
 # Iteration for counting tags, links (tlinks), wc (twords), total zettel (tzettel)
@@ -82,18 +59,6 @@ for filename in os.listdir(target_dir):
     if filename.endswith(".md"):
         file = open((os.path.join(target_dir, filename)), "r")
         data = file.read()
-        books = data.count("#book")
-        tbooks += books
-        blog = data.count("#blog-post")
-        tblogs += blog
-        article = data.count("#article")
-        tarticles += article
-        proof = data.count("#proofing")
-        tproof += proof
-        video = data.count("#video")
-        tvideo += video
-        twodo = data.count("#2do")
-        ttwodo += twodo
         links = data.count("]]")
         tlinks += links
         per_word = data.split()
@@ -161,45 +126,32 @@ for uuid in sorted(files, reverse=True):
 
 # Print and output
 
-output = f""" 
-## Memento Mori
+if args.s == 1:
+    output = f""" 
+    {'-'*40}
 
-Days since birth: {since.days}
-Days until I'm 80: {until.days}
-Days of COVID watch: {quarintine.days}
+    \t{twords} Total word count
+    \t{tlinks - tzettel} Total link count
+    \t{tzettel} Total zettel count
+    """
+elif args.rmd == 0:
+    output = f""" 
+    {'-'*40}
+    [{yesterday_count} new zettel yesterday.](thearchive://match/›[[{uuid}).
+    [{one_yr_ago_count} notes created on {one_yr_ago.strftime('%Y%m%d')}](thearchive://match/›[[{one_yr_ago.strftime('%Y%m%d')}).
+    [{two_yrs_ago_count} notes created on {two_yrs_ago.strftime('%Y%m%d')}](thearchive://match/›[[{two_yrs_ago.strftime('%Y%m%d')}).
+    {tencount} new zettel in {tengap} days.
+    {hundredcount} new zettel in {hundredgap} days.
+    {tzettel / (today - day0).days:.1f} zettel created on average since day zero.
+    {'-'*40}
 
-## Super Slogan
+    ## {tencount} Notes created in the last 10 days
 
-{ss}
-
-{'-'*40}
-
-\t{twords} Total word count
-\t{tlinks - tzettel} Total link count
-\t{tzettel} Total zettel count
-
-\t{tproof} Zettel Proofing
-\t{ttwodo} 2do's\n
-\t{tbooks} Books Processed
-\t{tblogs} Blog Posts
-\t{tarticles} Articles Processed
-\t{tvideo} Poetry of Zettelkasting videos
-
-{'-'*40}
-[{yesterday_count} new zettel yesterday.](thearchive://match/›[[{uuid}).
-[{one_yr_ago_count} notes created on {one_yr_ago.strftime('%Y%m%d')}](thearchive://match/›[[{one_yr_ago.strftime('%Y%m%d')}).
-[{two_yrs_ago_count} notes created on {two_yrs_ago.strftime('%Y%m%d')}](thearchive://match/›[[{two_yrs_ago.strftime('%Y%m%d')}).
-{tencount} new zettel in {tengap} days.
-{hundredcount} new zettel in {hundredgap} days.
-{tzettel / (today - day0).days:.1f} zettel created on average since day zero.
-{'-'*40}
-
-## {tencount} Notes created in the last 10 days
-
-"""
+    """
 
 for newnotes in tencountfiles:
     output += newnotes + '\n'
 
 print(output)
+print(args.echo)
 # pyperclip.copy(output)
