e07a8614c0d8cb70d3a13d55c3a3926c8449f4c1
Will Simpson <will@kestrelcreek.com>
2021-11-28 22:12:10 -0800

    New functions playground

---
+++ functions.py
@@ -0,0 +1,12 @@
+# # Formatting strings, with multiple integer variables:
+# stringVariable = 'I am {0} years old and I have {1} dollars in my pocket'
+# age = 19
+# cash = 65
+# print(stringVariable.format(age, cash))
+# Output: I am 19 years old and I have 65 dollars in my pocket
+
+
+def zettel(x):
+    yr_ago = datetime.now() - relativedelta(years=x)
+        if uuid == yr_ago.strftime('%Y%m%d'):
+            yr_ago_count += 1
