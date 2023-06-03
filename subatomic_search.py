0ad00a01894ee2896f59f27748d327f0f8997feb
Will Simpson <will@kestrelcreek.com>
2023-06-02 22:22:16 -0700

    Intital Commit

---
+++ subatomic_search.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# encoding: utf-8
+
+import os
+from zkfunctions import TheArchivePath
+
+def check(token, prefix):
+    directory = TheArchivePath()
+    results = []
+    search_terms = token.split()
+    for filename in os.listdir(directory):
+        if filename.endswith(".md"):
+            with open(os.path.join(directory, filename), 'r') as file:
+                for line_number, line in enumerate(file):
+                    if all(term in line for term in search_terms) and line.startswith(prefix):
+                        results.append((line, filename))
+    return results
+   
+if __name__ == "__main__":
+   lines = check('ChatGPT Google', 'Subatomic: ')
+if lines:
+        for line in lines:
+            result = ' '.join(line[0].split()[1:])
+            print(f'{result} ({line[1]})\n')
